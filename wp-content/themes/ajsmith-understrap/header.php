5d67f0b254f9de9c556abcc49d8551a8d89a2978
AJ <AJ@AJSmith.co.uk>
2017-04-08 16:52:39 +0100

    Nav bar in. Bckground sorted

--- wp-content/themes/ajsmith-understrap/header.php
+++ wp-content/themes/ajsmith-understrap/header.php
@@ -23,119 +23,102 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel='stylesheet' id='understrap-styles-css'  href='/AJSmith/wp-content/themes/ajsmith-understrap/sass/theme/_theme.css' type='text/css' media='all' />
 	<?php wp_head(); ?>
+	<link rel='stylesheet' id='understrap-styles-css'  href='/AJSmith/wp-content/themes/ajsmith-understrap/sass/theme/_header.css' type='text/css' media='all' />
+	<link rel='stylesheet' id='understrap-styles-css'  href='/AJSmith/wp-content/themes/ajsmith-understrap/sass/theme/_background-anim.css' type='text/css' media='all' />
+	<link rel='stylesheet' id='understrap-styles-css'  href='/AJSmith/wp-content/themes/ajsmith-understrap/sass/theme/_theme.css' type='text/css' media='all' />
+	<script type='text/javascript' src='/AJSmith/wp-content/themes/ajsmith-understrap/js/ajsmith.js'></script>
 </head>
 
 <body <?php body_class(); ?>>
 
+<ul class="background-slideshow">
+	<li><span></span></li>
+	<li><span></span></li>
+	<li><span></span></li>
+	<li><span></span></li>
+	<li><span></span></li>
+</ul>
 
-<!-- <style>.svg-clipped{-webkit-clip-path:url(#svgPath);clip-path:url(#svgPath);}</style>
-
-<div class="svg-clipped" style="background:url(/AJSmith/wp-content/themes/ajsmith-understrap/img/kayaking.jpg);height: 800px; width: 800px;"></div>
-
-<svg height="0" width="0" class="hero__title">
-	<defs>
-		<clippath id="svgPath">
-			<text x="0" y="300" textLength="690" lengthAdjust="spacing" font-family="christopherhand" font-size="210px" font-weight="700">AJ Smith</text>
-		</clippath>
-	</defs>
-</svg> -->
+<section class="navigation">
+  <div class="nav-container">
+    <!-- <div class="brand">
+      <a href="#!">Logo</a>
+    </div> -->
+    <nav>
+      <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
+      <ul class="nav-list">
+        <li class="hidden-desktop">
+          <a href="#!">Home</a>
+        </li>
+        <li>
+          <a href="#!"><i class="fa fa-code hidden-desktop" aria-hidden="true">&nbsp&nbsp</i>Front End Web Developer</a>
+        </li>
+        <!-- <li>
+          <a href="#!">Services</a>
+          <ul class="nav-dropdown">
+            <li>
+              <a href="#!">Web Design</a>
+            </li>
+            <li>
+              <a href="#!">Web Development</a>
+            </li>
+            <li>
+              <a href="#!">Graphic Design</a>
+            </li>
+          </ul>
+        </li> -->
+        <li>
+          <a href="#!"><i class="fa fa-camera hidden-desktop" aria-hidden="true">&nbsp&nbsp</i>Photographer</a>
+        </li>
+        <li>
+          <a href="#!"><i class="fa fa-phone hidden-desktop" aria-hidden="true">&nbsp&nbsp</i>Contact</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</section>
 
 <div class="hfeed site" id="page">
 
-	<!-- ******************* The Navbar Area ******************* -->
-	<div class="wrapper-fluid wrapper-navbar" id="wrapper-navbar">
-
-		<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content',
-		'understrap' ); ?></a>
-
-		<nav class="navbar navbar-toggleable-md  navbar-inverse bg-inverse">
-
-		<?php if ( 'container' == $container ) : ?>
-			<div class="container">
-		<?php endif; ?>
-
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-
-					<!-- Your site title as branding in the menu -->
-					<?php if ( ! has_custom_logo() ) { ?>
-
-						<?php if ( is_front_page() && is_home() ) : ?>
-
-							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-							
-						<?php else : ?>
-
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-						
-						<?php endif; ?>
-						
-					
-					<?php } else {
-						the_custom_logo();
-					} ?><!-- end custom logo -->
-
-				<!-- The WordPress Menu goes here -->
-				<?php wp_nav_menu(
-					array(
-						'theme_location'  => 'primary',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav',
-						'fallback_cb'     => '',
-						'menu_id'         => 'main-menu',
-						'walker'          => new WP_Bootstrap_Navwalker(),
-					)
-				); ?>
-			<?php if ( 'container' == $container ) : ?>
-			</div><!-- .container -->
-			<?php endif; ?>
-
-		</nav><!-- .site-navigation -->
-
-	</div><!-- .wrapper-navbar end -->
-
 	<div class="text-wrap">
-  <svg viewBox="0 0 500 80">
-    <pattern 
-             id="p-img1" 
-             viewBox="0 0 300 100"
-             patternUnits="userSpaceOnUse"
-             width="200%" height="200%"
-             x="-60%" y="-10%">
-      <image xlink:href="/AJSmith/wp-content/themes/ajsmith-understrap/img/iceberg.jpg" width="300" height="200"/>
-    </pattern>
-	<pattern 
-             id="p-img2" 
-             viewBox="0 0 300 100"
-             patternUnits="userSpaceOnUse"
-             width="200%" height="200%"
-             x="-60%" y="-10%">
-      <image xlink:href="/AJSmith/wp-content/themes/ajsmith-understrap/img/gorge.jpg" width="300" height="200"/>
-    </pattern>
-    <text text-anchor="middle"
-          x="50%"
-          y="50%"
-          dy=".35em"
-          class="img-layer">
-      AJ Smith
-    </text>
-    <!-- DISABLE GRADIENT
-	<linearGradient id="gr-overlay" x1="0" y1="0" x2="100%" y2="100%">
-      <stop stop-color="hsla(50, 100%, 70%, 0.5)" offset="10%"/>
-      <stop stop-color="hsla(200, 100%, 60%, 0.5)" offset="50%"/>
-      <stop stop-color="hsla(320, 100%, 50%, 0.5)" offset="90%"/>
-    </linearGradient>
+	  <svg viewBox="0 0 500 80">
+		<pattern 
+				 id="p-img1" 
+				 viewBox="0 0 300 100"
+				 patternUnits="userSpaceOnUse"
+				 width="200%" height="200%"
+				 x="-60%" y="-10%">
+		  <image xlink:href="/AJSmith/wp-content/themes/ajsmith-understrap/img/iceberg.jpg" width="300" height="200"/>
+		</pattern>
+		<pattern 
+				 id="p-img2" 
+				 viewBox="0 0 300 100"
+				 patternUnits="userSpaceOnUse"
+				 width="200%" height="200%"
+				 x="-60%" y="-10%">
+		  <image xlink:href="/AJSmith/wp-content/themes/ajsmith-understrap/img/gorge.jpg" width="300" height="200"/>
+		</pattern>
+		<text text-anchor="middle"
+			  x="50%"
+			  y="50%"
+			  dy=".35em"
+			  class="img-layer">
+		  AJ Smith
+		</text>
+		<!-- DISABLE GRADIENT
+		<linearGradient id="gr-overlay" x1="0" y1="0" x2="100%" y2="100%">
+		  <stop stop-color="hsla(50, 100%, 70%, 0.5)" offset="10%"/>
+		  <stop stop-color="hsla(200, 100%, 60%, 0.5)" offset="50%"/>
+		  <stop stop-color="hsla(320, 100%, 50%, 0.5)" offset="90%"/>
+		</linearGradient>
 
-    <text text-anchor="middle"
-          x="50%"
-          y="50%"
-          dy=".35em"
-          class="gradient-layer">
-      AJ Smith
-    </text> -->
-  </svg>
-</div>
+		<text text-anchor="middle"
+			  x="50%"
+			  y="50%"
+			  dy=".35em"
+			  class="gradient-layer">
+		  AJ Smith
+		</text> -->
+	  </svg>
+	</div>
